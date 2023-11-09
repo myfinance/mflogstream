2ea082d95d7b6d7c79c578182a7841e3be10bb84
holgerfischer <holger@hfischer.org>
2023-11-09 14:49:50 +0100

    push auditevents

--- src/main/java/de/hf/myfinance/logstream/LogstreamApplication.java
+++ src/main/java/de/hf/myfinance/logstream/LogstreamApplication.java
@@ -3,8 +3,10 @@ package de.hf.myfinance.logstream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("de.hf")
 public class LogstreamApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/hf/myfinance/logstream/events/MflogProcessorConfig.java
+++ src/main/java/de/hf/myfinance/logstream/events/MflogProcessorConfig.java
@@ -1,9 +1,9 @@
 package de.hf.myfinance.logstream.events;
 
+import de.hf.framework.audit.AuditService;
+import de.hf.framework.audit.Severity;
 import de.hf.myfinance.event.Event;
 import de.hf.myfinance.logstream.LogStreamWebSocketHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,34 +13,35 @@ import java.util.function.Consumer;
 
 @Configuration
 public class MflogProcessorConfig {
-    private static final Logger LOG = LoggerFactory.getLogger(MflogProcessorConfig.class);
+    private final AuditService auditService;
+    protected static final String AUDIT_MSG_TYPE="MflogProcessorC_Event";
 
     private LogStreamWebSocketHandler logStreamWebSocketHandler;
 
     @Autowired
-    public MflogProcessorConfig(LogStreamWebSocketHandler logStreamWebSocketHandler) {
+    public MflogProcessorConfig(LogStreamWebSocketHandler logStreamWebSocketHandler, AuditService auditService) {
         this.logStreamWebSocketHandler = logStreamWebSocketHandler;
+        this.auditService = auditService;
     }
 
     @Bean
     public Consumer<Event<String, String>> mflogProcessor() {
         return event -> {
-            LOG.info("Process message created at {}...", event.getEventCreatedAt());
-
+            auditService.saveMessage("Process message created at:"+event.getEventCreatedAt(), Severity.DEBUG, AUDIT_MSG_TYPE);
             switch (event.getEventType()) {
 
                 case CREATE:
-                    LOG.info("msg with ID: {}", event.getKey());
-                    LOG.info("msg: {}", event.getData());
+                    auditService.saveMessage("msg with ID:"+event.getKey(), Severity.DEBUG, AUDIT_MSG_TYPE);
+                    auditService.saveMessage("msg:"+event.getData(), Severity.DEBUG, AUDIT_MSG_TYPE);
                     this.logStreamWebSocketHandler.broadcastMessage(event.getKey() + ":" + event.getData());
                     break;
 
                 default:
                     String errorMessage = "Incorrect event type: " + event.getEventType() + ", expected a CREATE event";
-                    LOG.warn(errorMessage);
+                    auditService.saveMessage(errorMessage, Severity.WARN, AUDIT_MSG_TYPE);
             }
 
-            LOG.info("Message processing done!");
+            auditService.saveMessage("Message processing done!", Severity.DEBUG, AUDIT_MSG_TYPE);
 
         };
     }
